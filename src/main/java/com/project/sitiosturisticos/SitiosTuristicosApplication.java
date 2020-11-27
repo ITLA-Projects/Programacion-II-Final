3f6cabfa7b8c14621314fb961d4f4df952778695
Miguel Angel <miguelangelpena.dev@gmail.com>
2020-11-26 20:58:34 -0400

    first commit

---
+++ src/main/java/com/project/sitiosturisticos/SitiosTuristicosApplication.java
@@ -0,0 +1,13 @@
+package com.project.sitiosturisticos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitiosTuristicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitiosTuristicosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/sitiosturisticos/SitiosTuristicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.sitiosturisticos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitiosTuristicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
